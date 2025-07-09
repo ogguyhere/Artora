b3b67bffc0a115493e45ac93549610c23a73d095
ogguyhere <khadijahmfarooqi@gmail.com>
2025-07-09 15:29:02 +0500

    feat(added orders)

---
+++ frontend/src/pages/Orders.js
@@ -0,0 +1,11 @@
+// components/Orders.js
+import React from 'react';
+
+const Orders = () => (
+  <div>
+    <h4>📦 Orders</h4>
+    <p>You haven't placed any orders yet.</p>
+  </div>
+);
+
+export default Orders;
