24eea81158fdc88b89a7fcd9f5ff3fbbcd7f5dfc
ogguyhere <khadijahmfarooqi@gmail.com>
2025-07-09 15:30:08 +0500

    feat(added settings)

---
+++ frontend/src/pages/Settings.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+
+const Settings = () => (
+  <div>
+    <h4>⚙️ Settings</h4>
+    <p>Settings functionality coming soon.</p>
+  </div>
+);
+
+export default Settings;
